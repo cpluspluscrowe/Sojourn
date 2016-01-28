d900721febbcdf2adb5f618b8b8d31291e965c81
cpluspluscrowe <ccrowe@unomaha.edu>
2016-01-28 15:28:11 -0600

    ease enter the commit message for your changes. Lines starting 	modified:   budget/budget/wsgi.py

--- budget/budget/wsgi.py
+++ budget/budget/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/home/ccrowe2/Sojourn/budget/budget')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "budget.settings")
 
 application = get_wsgi_application()
