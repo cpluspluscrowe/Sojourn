f2322b5ded2ba3375c977a66209e76ff4ecb38b2
cpluspluscrowe <ccrowe@unomaha.edu>
2016-01-28 19:47:04 -0600

    modified:   budget/budget/settings.py

--- budget/budget/settings.py
+++ budget/budget/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proproject_db',
+        'USER': 'general',
+        'PASSWORD':'general',
     }
 }
 
