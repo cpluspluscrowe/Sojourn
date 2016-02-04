58a594652dc4e7a772230d3a6927cb17cfe7757a
cpluspluscrowe <ccrowe@unomaha.edu>
2016-02-04 00:30:44 -0600

    modified:   budget/budget/urls.py 	modified:   budget/.idea/workspace.xml 	modified:   budget/budget/__pycache__/settings.cpython-34.pyc 	modified:   budget/budget/__pycache__/urls.cpython-34.pyc 	modified:   budget/budget/__pycache__/wsgi.cpython-34.pyc 	modified:   budget/check_manager/__pycache__/models.cpython-34.pyc 	modified:   budget/check_manager/__pycache__/views.cpython-34.pyc 	modified:   budget/check_manager/models.py 	modified:   budget/check_manager/views.py 	modified:   budget/person_manager/__pycache__/models.cpython-34.pyc modified:   budget/person_manager/models.py 	modified:   budget/person_manager/views.py 	modified:   budget/templates/budget.html
    	budget/person_manager/__pycache__/views.cpython-34.pyc
    	budget/templates/add_user.html

--- budget/budget/urls.py
+++ budget/budget/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls import patterns
-import check_manager.views
 
 urlpatterns = patterns('',
-    url(r'^admin/$', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^budget/$',"check_manager.views.budget_sheet"),
      url(r'^budget/save/$',"check_manager.views.save_data"),
     url(r'^AddJS/$',"check_manager.views.add_js"),
+    url(r'^users/add$',"person_manager.views.add_person"),
+    url(r'users/save/$',"person_manager.views.save_person"),
                        )
 
